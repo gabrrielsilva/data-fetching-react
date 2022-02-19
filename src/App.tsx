11775f7a869d0bd8522d13a622b3104ba93d4471
gabriel <gabrielsilvaev@gmail.com>
2022-02-19 19:38:52 -0300

    without React Query

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { useFetch } from './hooks/useFetch';
+
+type Repository = {
+  full_name: string;
+  description: string;
+};
+
+function App() {
+  const { data: repositories, isFetching } = useFetch<Repository[]>(
+    '/users/gabrrielsilva/repos'
+  );
+
+  return (
+    <ul>
+      {isFetching && <p>Loading...</p>}
+      {repositories?.map((repo) => {
+        return (
+          <li key={repo.full_name}>
+            <strong>{repo.full_name}</strong>
+            <p>{repo.description}</p>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+export default App;
